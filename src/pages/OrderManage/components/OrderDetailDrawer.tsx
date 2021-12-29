2b0ae7d2ae62986738c30bf4994fc60031a9f4b9
大王 <king@gmail.com>
2021-12-29 23:42:25 +0800

    订单管理优化

---
+++ src/pages/OrderManage/components/OrderDetailDrawer.tsx
@@ -0,0 +1,149 @@
+import { payPlatformValueEnum } from '@/consts/valueEnums';
+import ProDescriptions from '@ant-design/pro-descriptions';
+import type { ProColumns } from '@ant-design/pro-table';
+import ProTable from '@ant-design/pro-table';
+import { Drawer, Tag } from 'antd';
+import { useEffect, useState } from 'react';
+import type { cartItemDTO, OrderDetailDTO } from '../data';
+import { orderDetail } from '../service';
+import styles from './style.less';
+
+type OrderDetailDrawerProps = {
+  visible: boolean;
+  orderId: number;
+
+  onCancel: () => void;
+};
+
+const cartColumns: ProColumns<cartItemDTO>[] = [
+  {
+    title: '商品名称',
+    dataIndex: 'gname',
+  },
+  {
+    title: '数量',
+    dataIndex: 'amount',
+  },
+  {
+    title: '原价',
+    dataIndex: 'price',
+    valueType: 'money',
+  },
+  {
+    title: '到手价',
+    dataIndex: 'realPrice',
+    valueType: 'money',
+  },
+  {
+    title: '打包费',
+    dataIndex: 'packageFee',
+  },
+];
+
+const OrderDetailDrawer: React.FC<OrderDetailDrawerProps> = (props) => {
+  const [detailLoading, setDetailLoading] = useState<boolean>(false);
+  const [userInfoDs, setUserInfoDs] = useState<any>();
+  const [payInfoDs, setPayInfoDs] = useState<any>();
+  const [cartDs, setCartDs] = useState<any>();
+
+  const detailRequest = async (id: number) => {
+    setDetailLoading(true);
+    const res = await orderDetail({ id: id });
+
+    const { orderAddressDTO, payment, cartDTOS, packageFee, deliveryFee } =
+      res.data as OrderDetailDTO;
+    setUserInfoDs({ ...orderAddressDTO });
+    setPayInfoDs({ ...payment, packageFee, deliveryFee });
+    setCartDs(cartDTOS);
+
+    setDetailLoading(false);
+  };
+
+  useEffect(() => {
+    const fn = async (orderId: number) => {
+      await detailRequest(orderId);
+    };
+
+    if (props.orderId) {
+      fn(props.orderId);
+    }
+  }, [props.orderId]);
+
+  return (
+    <>
+      <Drawer
+        width={800}
+        visible={props.visible}
+        title={'订单详情'}
+        onClose={() => {
+          props.onCancel();
+        }}
+        closable={true}
+      >
+        <ProDescriptions<any>
+          column={3}
+          title={'用户信息'}
+          dataSource={userInfoDs}
+          loading={detailLoading}
+        >
+          <ProDescriptions.Item
+            dataIndex="contacts"
+            label="姓名"
+            renderText={(_, record) => `${record.contacts}(${record.gender})`}
+          />
+          <ProDescriptions.Item dataIndex="phone" label="手机号" />
+          <ProDescriptions.Item
+            dataIndex="contacts"
+            label="联系地址"
+            render={(_, record) => {
+              return (
+                <>
+                  <Tag color="processing">{record.label}</Tag>
+                  {record.address} {record.building} {record.houseNumber}
+                </>
+              );
+            }}
+          />
+        </ProDescriptions>
+        <ProDescriptions<any>
+          column={3}
+          title={'支付信息'}
+          dataSource={payInfoDs}
+          loading={detailLoading}
+        >
+          <ProDescriptions.Item dataIndex="createTime" label="支付时间" valueType={'dateTime'} />
+          <ProDescriptions.Item dataIndex="outTradeNo" label="商户订单号" />
+          <ProDescriptions.Item
+            dataIndex="payType"
+            label="支付方式"
+            valueEnum={payPlatformValueEnum}
+          />
+          <ProDescriptions.Item dataIndex="packageFee" label="打包费" valueType={'money'} />
+          <ProDescriptions.Item dataIndex="deliveryFee" label="配送费" valueType={'money'} />
+          <ProDescriptions.Item
+            dataIndex="fee"
+            label="支付金额"
+            renderText={(_, record) => {
+              const fenFee = record.fee as number;
+              return '￥' + (fenFee / 100).toFixed(2);
+            }}
+          />
+        </ProDescriptions>
+        <div className={styles.title}>商品明细</div>
+        <ProTable
+          style={{ marginBottom: 24 }}
+          pagination={false}
+          search={false}
+          loading={detailLoading}
+          options={false}
+          toolBarRender={false}
+          dataSource={cartDs}
+          columns={cartColumns}
+          rowKey="gid"
+        />
+      </Drawer>
+    </>
+  );
+};
+
+export default OrderDetailDrawer;
--- src/pages/OrderManage/index.tsx
+++ src/pages/OrderManage/index.tsx
@@ -1,20 +1,15 @@
-import { Drawer, Popover, Tag } from 'antd';
+import { Popover } from 'antd';
 import React, { useState } from 'react';
 import { PageContainer } from '@ant-design/pro-layout';
 import type { ProColumns } from '@ant-design/pro-table';
 import ProTable, { TableDropdown } from '@ant-design/pro-table';
-import ProDescriptions from '@ant-design/pro-descriptions';
-import { orderPageInfo, orderDetail } from './service';
-import type { TableListItem, TableListPagination, OrderDetailDTO, cartItemDTO } from './data';
-import styles from './style.less';
-import {
-  orderStatusValueEnum,
-  payPlatformValueEnum,
-  payStatusValueEnum,
-} from '@/consts/valueEnums';
+import { orderPageInfo } from './service';
+import type { TableListItem, TableListPagination } from './data';
+import { orderStatusValueEnum, payStatusValueEnum } from '@/consts/valueEnums';
 import { buildingPageInfo } from '../biz/BuildingManage/service';
 import type { RequestOptionsType } from '@ant-design/pro-utils';
 import { MessageOutlined } from '@ant-design/icons';
+import OrderDetailDrawer from './components/OrderDetailDrawer';
 
 const tableRequest = async (params?: { pageSize: number; current: number }) => {
   const res = await orderPageInfo({
@@ -38,26 +33,8 @@ const buildingSelectRequest = async () => {
 };
 
 const TableList: React.FC = () => {
-  const [showDetail, setShowDetail] = useState<boolean>(false);
-  const [userInfoDs, setUserInfoDs] = useState<any>();
-  const [payInfoDs, setPayInfoDs] = useState<any>();
-  const [cartDs, setCartDs] = useState<any>();
-  const [detailLoading, setDetailLoading] = useState<boolean>(true);
-
-  const detailRequest = async (id: number) => {
-    setDetailLoading(true);
-    const res = await orderDetail({ id: id });
-
-    const { orderAddressDTO, payment, cartDTOS, packageFee, deliveryFee } =
-      res.data as OrderDetailDTO;
-    setUserInfoDs({ ...orderAddressDTO });
-    setPayInfoDs({ ...payment, packageFee, deliveryFee });
-    setCartDs(cartDTOS);
-
-    setDetailLoading(false);
-  };
-
-  /** 国际化配置 */
+  const [orderDetailVisible, setOrderDetailVisible] = useState<boolean>(false);
+  const [currentRow, setCurrentRow] = useState<any>();
 
   const columns: ProColumns<TableListItem>[] = [
     {
@@ -162,10 +139,8 @@ const TableList: React.FC = () => {
         <a
           key="order_detail"
           onClick={async () => {
-            // setCurrentRow(record);
-            setShowDetail(true);
-            // console.log(record);
-            await detailRequest(record?.id as number);
+            setCurrentRow(record);
+            setOrderDetailVisible(true);
           }}
         >
           详情
@@ -188,31 +163,6 @@ const TableList: React.FC = () => {
     },
   ];
 
-  const cartColumns: ProColumns<cartItemDTO>[] = [
-    {
-      title: '商品名称',
-      dataIndex: 'gname',
-    },
-    {
-      title: '数量',
-      dataIndex: 'amount',
-    },
-    {
-      title: '原价',
-      dataIndex: 'price',
-      valueType: 'money',
-    },
-    {
-      title: '到手价',
-      dataIndex: 'realPrice',
-      valueType: 'money',
-    },
-    {
-      title: '打包费',
-      dataIndex: 'packageFee',
-    },
-  ];
-
   return (
     <PageContainer>
       <ProTable<TableListItem, TableListPagination>
@@ -226,77 +176,11 @@ const TableList: React.FC = () => {
         rowSelection={false}
       />
 
-      <Drawer
-        width={800}
-        visible={showDetail}
-        title={'订单详情'}
-        onClose={() => {
-          setShowDetail(false);
-        }}
-        closable={true}
-      >
-        <ProDescriptions<any>
-          column={3}
-          title={'用户信息'}
-          dataSource={userInfoDs}
-          loading={detailLoading}
-        >
-          <ProDescriptions.Item
-            dataIndex="contacts"
-            label="姓名"
-            renderText={(_, record) => `${record.contacts}(${record.gender})`}
-          />
-          <ProDescriptions.Item dataIndex="phone" label="手机号" />
-          <ProDescriptions.Item
-            dataIndex="contacts"
-            label="联系地址"
-            render={(_, record) => {
-              return (
-                <>
-                  <Tag color="processing">{record.label}</Tag>
-                  {record.address} {record.building} {record.houseNumber}
-                </>
-              );
-            }}
-          />
-        </ProDescriptions>
-        <ProDescriptions<any>
-          column={3}
-          title={'支付信息'}
-          dataSource={payInfoDs}
-          loading={detailLoading}
-        >
-          <ProDescriptions.Item dataIndex="createTime" label="支付时间" valueType={'dateTime'} />
-          <ProDescriptions.Item dataIndex="outTradeNo" label="商户订单号" />
-          <ProDescriptions.Item
-            dataIndex="payType"
-            label="支付方式"
-            valueEnum={payPlatformValueEnum}
-          />
-          <ProDescriptions.Item dataIndex="packageFee" label="打包费" valueType={'money'} />
-          <ProDescriptions.Item dataIndex="deliveryFee" label="配送费" valueType={'money'} />
-          <ProDescriptions.Item
-            dataIndex="fee"
-            label="支付金额"
-            renderText={(_, record) => {
-              const fenFee = record.fee as number;
-              return '￥' + (fenFee / 100).toFixed(2);
-            }}
-          />
-        </ProDescriptions>
-        <div className={styles.title}>商品明细</div>
-        <ProTable
-          style={{ marginBottom: 24 }}
-          pagination={false}
-          search={false}
-          loading={detailLoading}
-          options={false}
-          toolBarRender={false}
-          dataSource={cartDs}
-          columns={cartColumns}
-          rowKey="gid"
-        />
-      </Drawer>
+      <OrderDetailDrawer
+        visible={orderDetailVisible}
+        orderId={currentRow?.id}
+        onCancel={() => setOrderDetailVisible(false)}
+      />
     </PageContainer>
   );
 };
