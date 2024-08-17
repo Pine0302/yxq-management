4cd93a8f821b9fc557797e8a2120c48b5b82ca0f
ning.zhang <feifandeni@163.com>
2024-08-17 19:29:57 +0800

    代码提交

--- src/pages/live/KitchenLive/components/MergeForm.tsx
+++ src/pages/live/KitchenLive/components/MergeForm.tsx
@@ -5,8 +5,9 @@ import { message } from 'antd';
 import type { RequestOptionsType } from '@ant-design/pro-utils';
 import { kitchenPageInfo } from '../../../biz/KitchenManage/service';
 import { addKitchenLive, updateKitchenLive } from '../service';
-import { Upload, Modal } from 'antd';
+import { Upload, Modal, Tabs, Radio, Form, Input } from 'antd';
 import type { UploadFile, RcFile } from 'antd/lib/upload/interface';
+import '../style.less';
 import 'antd/dist/antd.css'; // 确保正确导入antd样式
 import 'antd/es/upload/style/index.css';
 
@@ -56,15 +57,88 @@ const getBase64 = (file: RcFile | undefined) => {
   });
 };
 
+type TabProps = {
+  form: DataItem;
+  parentCallback: (data: DataItem) => void;
+  sourceKey: 'source1' | 'source2';
+  inputKey: 'weixinLiveId' | 'liveParams';
+};
+
+const Tab: React.FC<TabProps> = ({ form, parentCallback, sourceKey, inputKey }) => {
+  const [tabForm, setTabForm] = useState<DataItem>({
+    source1: 'val1',
+    source2: 'val2',
+    weixinLiveId: '',
+    liveParams: '',
+  });
+
+  useEffect(() => {
+    if (!form.source1) {
+      form.source1 = 'val1';
+    }
+    if (!form.source2) {
+      form.source2 = 'val2';
+    }
+    setTabForm({
+      ...form,
+    });
+  }, [form]);
+  const sendDataToParent = () => {
+    parentCallback(tabForm);
+  };
+
+  useEffect(() => {
+    sendDataToParent();
+  }, [tabForm[inputKey]]);
+
+  const handleInputChange = (value: string) => {
+    setTabForm((prevTabForm) => ({
+      ...prevTabForm,
+      [inputKey]: value,
+    }));
+  };
+
+  return (
+    <>
+      <div>
+        <label className="label-box">直播间来源：</label>
+        <Radio.Group disabled value={tabForm[sourceKey]}>
+          <Radio value="val1"> 微信视频号直播间 </Radio>
+          <Radio value="val2"> 其他直播间 </Radio>
+        </Radio.Group>
+      </div>
+      <div>
+        <label className="label-box">
+          {inputKey === 'weixinLiveId' ? '微信视频号ID' : '直播参数'}：
+        </label>
+        <Input
+          placeholder={`请输入${inputKey === 'weixinLiveId' ? '微信视频号ID' : '直播参数'}`}
+          style={{ width: 200 }}
+          value={tabForm[inputKey]}
+          onChange={(e) => handleInputChange(e.target.value)}
+        />
+      </div>
+    </>
+  );
+};
 const MergeForm: React.FC<MergeFormProps> = (props) => {
   const formRef = useRef<FormInstance<DataItem>>();
   const [fileList, setFileList] = useState<UploadFile[]>([]);
   const [previewImage, setPreviewImage] = useState<string>('');
   const [previewVisible, setPreviewVisible] = useState<boolean>(false);
+  const [fullForm, setFullForm] = useState<DataItem>({});
+  const [childData, setChildData] = useState({});
+
+  const handleCallback = (data) => {
+    setChildData({ ...childData, ...data });
+  };
 
   useEffect(() => {
     if (props?.value) {
       formRef.current?.setFieldsValue(props.value);
+      //回显
+      setFullForm(props.value);
+
       if (props.value.cover) {
         setFileList([
           {
@@ -78,12 +152,14 @@ const MergeForm: React.FC<MergeFormProps> = (props) => {
     } else {
       formRef.current?.resetFields();
       setFileList([]);
+      setFullForm({});
     }
 
     return () => {
       setFileList([]);
       setPreviewVisible(false);
       setPreviewImage('');
+      setFullForm({});
     };
   }, [props.value]);
 
@@ -101,12 +177,48 @@ const MergeForm: React.FC<MergeFormProps> = (props) => {
     setFileList(newFileList);
   };
 
+  const items = [
+    {
+      key: '1',
+      label: 'H5小程序端',
+      children: (
+        <Tab
+          form={fullForm}
+          parentCallback={handleCallback}
+          sourceKey="source1"
+          inputKey="weixinLiveId"
+        />
+      ),
+    },
+    {
+      key: '2',
+      label: 'APP端',
+      children: (
+        <Tab
+          form={fullForm}
+          parentCallback={handleCallback}
+          sourceKey="source2"
+          inputKey="liveParams"
+        />
+      ),
+    },
+  ];
+
   const columns: ProFormColumnsType<DataItem>[] = [
     {
       title: 'id',
       dataIndex: 'id',
       formItemProps: {
         hidden: true,
+      },
+    },
+    {
+      title: '',
+      renderFormItem: () => {
+        return <div className="text-title text-bg">基础信息</div>;
+      },
+      formItemProps: {
+        className: 'text-box',
       },
     },
     {
@@ -117,14 +229,14 @@ const MergeForm: React.FC<MergeFormProps> = (props) => {
       },
     },
     {
-      title: '排序',
+      title: '直播间排序',
       dataIndex: 'sort',
       formItemProps: {
         rules: [{ required: true }],
       },
     },
     {
-      title: '所在厨房',
+      title: '所属厨房',
       dataIndex: 'kitchenId',
       valueType: 'select',
       width: '100%',
@@ -134,9 +246,8 @@ const MergeForm: React.FC<MergeFormProps> = (props) => {
       },
     },
     {
-      title: '封面图',
+      title: '默认展示图',
       dataIndex: 'cover',
-      valueType: 'formItem',
       renderFormItem: (_, { type, defaultRender, ...rest }, form) => {
         return (
           <>
@@ -156,6 +267,29 @@ const MergeForm: React.FC<MergeFormProps> = (props) => {
         );
       },
     },
+    {
+      title: '',
+      renderFormItem: () => {
+        return (
+          <>
+            <div className="text-title text-bg">直播间参数设置</div>
+            <Tabs
+              type="card"
+              items={items.map((item) => {
+                return {
+                  label: item.label,
+                  key: item.key,
+                  children: item.children,
+                };
+              })}
+            />
+          </>
+        );
+      },
+      formItemProps: {
+        className: 'text-box',
+      },
+    },
   ];
 
   return (
@@ -163,18 +297,22 @@ const MergeForm: React.FC<MergeFormProps> = (props) => {
       <BetaSchemaForm<DataItem>
         title={props?.isEdit ? '编辑厨房直播摄像头' : '新增厨房直播摄像头'}
         formRef={formRef}
-        width={500}
+        width={800}
         layout="horizontal"
         layoutType="ModalForm"
-        labelCol={{ span: 6 }}
+        labelCol={{ span: 4 }}
         wrapperCol={{ span: 22 }}
         onFinish={async (values) => {
+          // const formData = {
+          //   ...values,
+          //   cover: fileList.length > 0 ? fileList[0].thumbUrl || fileList[0].url : '',
+          // };
           const formData = {
             ...values,
-            cover: fileList.length > 0 ? fileList[0].thumbUrl || fileList[0].url : '',
+            ...childData,
           };
           console.log(formData);
-          await handleSubmit(values, props?.isEdit);
+          await handleSubmit(formData, props?.isEdit);
           message.success('操作成功');
           props?.onSuccess?.();
           return true;
