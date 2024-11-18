82ddf213bc9b1b279541ce9bac9ea1412b965455
Pine <7838472+lazypine@user.noreply.gitee.com>
2024-11-18 15:33:48 +0800

    管理楼宇商品

--- src/pages/biz/BuildingManage/components/AreaGoodstemplateForm.tsx
+++ src/pages/biz/BuildingManage/components/AreaGoodstemplateForm.tsx
@@ -1,11 +1,18 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { PlusOutlined } from '@ant-design/icons';
 import { Form, Input, Table, Drawer, Button, Select, TimePicker, Modal, message } from 'antd';
+import { ModalForm, ProFormText } from '@ant-design/pro-form';
+
+import { InputNumber, Popconfirm } from 'antd';
+
 import {
   fetchAreaGoodsTemplate,
+  editAreaGoodsTemplate,
   addAddressTemplate,
   editAddressTemplate,
   deleteAddressTemplate,
+  editAreaStock,
+  downSell,
 } from '../service';
 import type {
   TemlateAddressTableItem,
@@ -17,6 +24,8 @@ import { PageContainer } from '@ant-design/pro-layout';
 import ProTable from '@ant-design/pro-table';
 import CreateBuildingForm from './CreateBuildingForm'; // 确保路径正确
 import SubAddressTemplateForm from './SubAddresstemplateForm'; // 确保路径正确
+import EditAreaGoodsForm from './EditAreaGoodsForm'; // 确保路径正确
+import { Stock } from '@ant-design/charts';
 
 interface AreaGoodsTemplateFormProps {
   visible: boolean;
@@ -33,6 +42,15 @@ interface BuildingFormValues {
   isEdit?: boolean;
 }
 
+type DataItem = {
+  id: number;
+  gid: number;
+  gname: string;
+  total: number;
+  sold: number;
+  newStock: number;
+};
+
 const AreaGoodsTemplateForm: React.FC<AreaGoodsTemplateFormProps> = ({
   visible,
   onCancel,
@@ -40,10 +58,16 @@ const AreaGoodsTemplateForm: React.FC<AreaGoodsTemplateFormProps> = ({
 }) => {
   const actionRef = useRef<ActionType>();
   const [modalVisible, setModalVisible] = useState(false);
+  const [subModalVisible, setSubModalVisible] = useState(false);
   const [form] = Form.useForm();
   const [currentRecord, setCurrentRecord] = useState<BuildingFormValues | null>(null);
   const [subDrawerVisible, setSubDrawerVisible] = useState<boolean>(false); // 控制subDrawer的显示
   const [subKey, setSubKey] = useState(0);
+  const [ds, setDs] = useState<DataItem[]>([]);
+  const [inputValue, setInputValue] = useState<number | null>(null);
+  const setKitchenStock = async (kitchenGoodsId: number, newStock: number) => {
+    return await editAreaStock({ id: kitchenGoodsId, stock: newStock });
+  };
 
   const handleCreate = async (values: BuildingFormValues) => {
     console.log('Form values:', values);
@@ -73,6 +97,27 @@ const AreaGoodsTemplateForm: React.FC<AreaGoodsTemplateFormProps> = ({
     setModalVisible(true); // 打开编辑楼栋的模态框
   };
 
+  // 使用POST方法删除数据的函数
+  const handleEnd = (record: any) => {
+    Modal.confirm({
+      title: '下架后商品将从用户侧隐藏，但不会影响正在交易中的商品（已付款的商品）',
+      content: '确认下架该商品？',
+      okText: '确认',
+      okType: 'danger',
+      cancelText: '取消',
+      onOk: async () => {
+        try {
+          const postData = { id: record.id, stock: record.total };
+          const res = await downSell(postData);
+          message.success('下架成功');
+          actionRef.current?.reload(); // 刷新表格数据
+        } catch (error) {
+          message.error('下架操作失败: ' + error.message);
+        }
+      },
+    });
+  };
+
   useEffect(() => {
     console.log('currentRecord-------:', currentRecord);
     if (!subDrawerVisible && currentRecord && currentRecord.isEdit == false) {
@@ -83,14 +128,14 @@ const AreaGoodsTemplateForm: React.FC<AreaGoodsTemplateFormProps> = ({
 
   const handleEditDone = async (values: TemlateAddressTableItem) => {
     console.log('Form values for edit:', values);
-    const postData = { id: values.id, ...values };
+    const postData = { id: values.id, status: true, ...values };
     console.log('Form values for edit:', postData);
     // 可以在这里添加提交到服务器的代码
-    const response = await editAddressTemplate(postData);
+    const response = await editAreaGoodsTemplate(postData);
     if (response && response.success) {
-      message.success('地址模板修改成功');
+      message.success('楼宇商品更新成功');
       values.isEdit = false;
-      setModalVisible(false); // 关闭模态框
+      setSubModalVisible(false); // 关闭模态框
       actionRef.current?.reload(); // 刷新ProTable
     } else {
       setModalVisible(true);
@@ -102,7 +147,7 @@ const AreaGoodsTemplateForm: React.FC<AreaGoodsTemplateFormProps> = ({
     console.log('Opening sub-address management for:', record);
     setModalVisible(false); // 确保关闭任何已经打开的编辑模态框
     setCurrentRecord(record); // 更新当前记录
-    setSubDrawerVisible(true);
+    setSubModalVisible(true);
   };
 
   const handleDelete = async (record: TemlateAddressTableItem) => {
@@ -191,21 +236,66 @@ const AreaGoodsTemplateForm: React.FC<AreaGoodsTemplateFormProps> = ({
       title: '操作',
       dataIndex: 'option',
       valueType: 'option',
-      render: (_, record) => [
-        <a key="edit" onClick={() => handleEdit(record)}>
-          编辑
-        </a>,
-        <a
-          key="delete"
-          onClick={() => handleDelete(record)}
-          style={{ color: 'red', marginLeft: 8 }}
-        >
-          删除
-        </a>,
-        <a key="subAddressTemplate" onClick={() => handleSubAddressManagement(record)}>
-          商品管理
-        </a>,
-      ],
+      render: (_, record) => {
+        if (!record.originStatus) {
+          // 商品是禁用状态，不允许任何操作
+          return null;
+        }
+
+        if (record.status === false) {
+          return [
+            <a key="subAddressTemplate" onClick={() => handleSubAddressManagement(record)}>
+              上架
+            </a>,
+          ];
+        } else {
+          return [
+            <a key="edit" onClick={() => handleEnd(record)}>
+              下架
+            </a>,
+            <Popconfirm
+              key={`ppp- ${record.id}`}
+              icon={<></>}
+              title={
+                <InputNumber
+                  min={0}
+                  value={inputValue ?? record.total} // 使用 inputValue 或 fallback 为 record.total
+                  controls={true}
+                  onChange={(value) => {
+                    setInputValue(value); // 更新受控状态
+                    const updatedData = ds.map((item) =>
+                      item.id === record.id ? { ...item, total: value } : item,
+                    );
+                    setDs(updatedData);
+                  }}
+                />
+              }
+              onOpenChange={(open) => {
+                if (open) {
+                  // 当 Popconfirm 打开时，重置为当前 record.total
+                  setInputValue(record.total);
+                }
+              }}
+              onConfirm={async () => {
+                const res = await setKitchenStock(record.id, inputValue); // 使用更新后的值
+                if (res.code === 200) {
+                  message.success('操作成功.');
+                  setDs((prevDs) => {
+                    return prevDs.map((item) =>
+                      item.id === record.id ? { ...item, total: inputValue ?? record.total } : item,
+                    );
+                  });
+                  actionRef.current?.reload(); // 强制刷新表格数据
+                }
+              }}
+              okText="保存"
+              cancelText="取消"
+            >
+              <a href="#">调库存</a>
+            </Popconfirm>,
+          ];
+        }
+      },
     },
   ];
 
@@ -240,6 +330,7 @@ const AreaGoodsTemplateForm: React.FC<AreaGoodsTemplateFormProps> = ({
           </Button>,
         ]}
         request={tableRequest}
+        //dataSource={ds}
         columns={columns}
         // rowSelection={{
         //   onChange: (_, selectedRows) => {
@@ -253,6 +344,12 @@ const AreaGoodsTemplateForm: React.FC<AreaGoodsTemplateFormProps> = ({
         visible={modalVisible}
         onCreate={currentRecord ? handleEditDone : handleCreate}
         onCancel={() => setModalVisible(false)}
+        initialValues={currentRecord}
+      />
+      <EditAreaGoodsForm
+        visible={subModalVisible}
+        onCreate={currentRecord ? handleEditDone : handleCreate}
+        onCancel={() => setSubModalVisible(false)}
         initialValues={currentRecord}
       />
 
